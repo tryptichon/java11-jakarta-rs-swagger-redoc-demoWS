813680c4fe688745699cd263819dc226fa54be45
tryptichon <duke01@gmx.net>
2022-04-22 22:05:45 +0200

    First import

---
+++ README.md
@@ -0,0 +1,9 @@
+# DEMO WS
+
+An Jakarta-JAX-RS skeleton webapp using:
+
+* Jakarta-JAX-RS with annotations.
+* Swagger annotations for automatic openapi.json/.yaml generation.
+* ReDoc standalone to generate a documentation website for the api.
+
+Best to be used with IntelliJ IDEA and Java 11.
---
+++ src/main/java/com/example/demows/HelloResource.java
@@ -0,0 +1,41 @@
+package com.example.demows;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    @Operation(
+            summary = "Get simple string result",
+            tags = {"Tag 1"},
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Normal response",
+                            content = @Content(
+                                    mediaType = "text/plain",
+                                    schema = @Schema(implementation = String.class),
+                                    examples = {
+                                            @ExampleObject(
+                                                    name = "Default result",
+                                                    summary = "Example of default result",
+                                                    value = "Hello World!"
+                                            )
+                                    }
+                            )
+                    )
+            }
+    )
+    public String hello() {
+        return "Hello, World!";
+    }
+}
